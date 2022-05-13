bc0d5338113153d5151d6d1f01c7358323bc327c
Milos Milutinovic <milos.milutinovic35@yahoo.com>
2022-05-13 21:18:45 +0200

    Update file name

--- osnove_jave/src/main/java/p13_05_2022/tekstovi_zadataka/txt.java
+++ osnove_jave/src/main/java/p13_05_2022/tekstovi_zadataka/txt.java
@@ -8,5 +8,13 @@ public class txt {
                 "Commit-ujte ovo kao promenu “Predavanja 13.05 Git Zadatak 1” \n" +
                 "Push-ujte na github\n" +
                 "\n");
+
+        System.out.println("Git zadatak\n" +
+                "Preimenovati fajl Git_Zadatak1.java u Git_Zadatak1_updated. Napomena: To radimo u intelij preko refactor\n" +
+                "Dodajte tekst zadataka u tekstovi_zadataka.txt\n" +
+                "Stage-ujte sve promene\n" +
+                "Commitujte porukom “Update file name”\n" +
+                "Push-ujte na github\n");
+
     }
 }
