169483059bcddb920685663e18d9e40b668f00a3
Milos Milutinovic <milos.milutinovic35@yahoo.com>
2022-05-13 20:33:27 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/tekstovi_zadataka/txt.java
@@ -0,0 +1,12 @@
+package p13_05_2022.tekstovi_zadataka;
+
+public class txt {
+    public static void main(String[] args) {
+        System.out.println("Git zadatak\n" +
+                "Kreirati novi fajl u paketu sa danasnjeg predavanja. Fajl se zove tekstovi_zadataka.txt i u njemu pejstujte tekst sa ovog zadataka.\n" +
+                "Kreirati novu java klasu Git_Zadatak1.java i u njoj ispisite na ekranu sve git naredbe koje smo radili.\n" +
+                "Commit-ujte ovo kao promenu “Predavanja 13.05 Git Zadatak 1” \n" +
+                "Push-ujte na github\n" +
+                "\n");
+    }
+}
